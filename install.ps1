ca23cd9c8b84366b1dd2aa4326ceffcd1d03b8c8
rico1202 <rico1202@gmail.com>
2024-10-16 18:06:49 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,14 +1,2 @@
 # Install Notepad++
 winget install --id Notepad++.Notepad++ -e --accept-package-agreements --accept-source-agreements
-
-# Install VLC Media Player
-winget install --id VideoLAN.VLC -e --accept-package-agreements --accept-source-agreements
-
-# Install Adobe Acrobat Reader
-winget install --id Adobe.Acrobat.Reader.64-bit -e --accept-package-agreements --accept-source-agreements
-
-# Install 7-Zip
-winget install --id 7zip.7zip -e --accept-package-agreements --accept-source-agreements
-
-# Install HWMonitor
-winget install --id CPUID.HWMonitor -e --accept-package-agreements --accept-source-agreements
