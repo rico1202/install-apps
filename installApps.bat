f34367f513d4b9615d78d096f1b71e8eb67db23b
rico1202 <rico1202@gmail.com>
2024-10-16 18:20:26 +0200

    Add files via upload

---
+++ installApps.bat
@@ -0,0 +1,2 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "iex (iwr 'https://raw.githubusercontent.com/rico1202/install-apps/refs/heads/main/install.ps1' -UseBasicParsing).Content"
+PAUSE
